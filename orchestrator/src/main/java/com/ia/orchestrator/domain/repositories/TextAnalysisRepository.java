95a7fe54ae4eb571e0a9ce1bdef60de5f8b1e28f
Jorge Luiz Andrade <luizj1718@gmail.com>
2026-01-28 18:48:47 -0300

    style: mudando o nome colocando no padrão de Interface

--- orchestrator/src/main/java/com/ia/orchestrator/domain/repositories/TextAnalysisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ia.orchestrator.domain.repositories;
-
-import com.ia.orchestrator.domain.TextAnalysis;
-
-public interface TextAnalysisRepository {
-	TextAnalysis save(TextAnalysis analysis);
-}
